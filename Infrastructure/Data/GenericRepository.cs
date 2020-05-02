9cc22f8f234860ad2b5d736e7b14ae0c94ec3373
Vinay <mohitevinay910@gmail.com>
2020-05-02 23:42:25 +0530

    End of Section 6

--- Infrastructure/Data/GenericRepository.cs
+++ Infrastructure/Data/GenericRepository.cs
@@ -16,6 +16,7 @@ namespace Infrastructure.Data
             _context = context;
         }
 
+        
         public async Task<T> GetByIdAsync(int id)
         {
              return await _context.Set<T>().FindAsync(id);
@@ -36,9 +37,15 @@ namespace Infrastructure.Data
             return await ApplySpecification(spec).ToListAsync();
         }
 
+        public async Task<int> CountAsync(ISpecification<T> spec)
+        {
+            return await ApplySpecification(spec).CountAsync();
+        }
         private IQueryable<T> ApplySpecification(ISpecification<T> spec)
         {
             return SpecificationEvaluator<T>.GetQuery(_context.Set<T>().AsQueryable(), spec);
         }
+
+        
     }
 }
